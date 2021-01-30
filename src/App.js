46f60144157b52f17b3169ec5c798b0130efa37b
OmerYesilkaya <omeryesilkaya@std.sehir.edu.tr>
2021-01-30 19:11:13 +0300

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Flex } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Flex w="100vw" h="100vh">
+			test
+		</Flex>
+	);
 }
 
 export default App;
